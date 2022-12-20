6a75aba24d109720a234ebcf8c18d2f6bf0a6cbb
James Vardy <james.vardyc@gmail.com>
2022-12-20 11:16:47 +0000

    Add files via upload

--- 7/7.go
+++ 7/7.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 )
 
 var commandList []string
+var nestingLevel = 0
+var tree []string
 
 func main() {
 
+	totalLines := 0
+	parentDirectory := ""
+	dirSizes := make(map[string]int)
+	dirSizes[parentDirectory] = 0
+	
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -21,16 +27,43 @@ func main() {
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
 		commandList = append(commandList, scanner.Text())
+		totalLines++
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(commandList)
+	parentDirectory, size = handleCommand(0, parentDirectory)
+
+	for i := 0; i < len(tree); i++ {
+		println(tree[i] + "\n")
+	}
 
 }
 
-func handleCd(dir string) {
+func handleCommand(lineNum int, parentDirectory string)(string, int) {
+
+	if (commandList[lineNum][4:] == "$ cd") {
+		var dir = commandList[lineNum][5:]
+		if dir == ".." {
+			nestingLevel--
+		} else if (dir == "/") {
+			nestingLevel = 0
+		}  else {
+			tempStr := ""
+			for n := 0; n < nestingLevel; n++ {
+				tempStr += "-"
+			}
+			tempStr += dir
+			tree = append(tree, tempStr)
+			nestingLevel++
+		} 
+	}
+	
+
+
+	return parentDirectory
 
 }
+
